3f6f4f7cb72f2cbf7baa8992841457d7e7890d97
tobyzl2 <tobyzl2@illinois.edu>
2019-02-01 22:50:42 -0600

    Rabin-Karp implemented and added simple test case.

--- src/RabinKarp.java
+++ src/RabinKarp.java
@@ -1,7 +1,24 @@
 public class RabinKarp {
     //Source: https://www.coursera.org/lecture/algorithms-part2/rabin-karp-3KiqT
-    static int indexOf(String pattern, String text) {
-        return 0;
+    static int indexOf(String pattern, String text) throws IllegalArgumentException {
+        if (pattern == null || text == null || pattern.length() > text.length()) {
+            throw new IllegalArgumentException();
+        }
+
+        long patternHash = getHash(pattern);
+        for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
+            String textFragment = text.substring(i, pattern.length() + i);
+            long textFragmentHash = getHash(textFragment);
+            if (textFragmentHash == patternHash) {
+                for (int j = 0; j < text.length(); j++) {
+                    if (text.charAt(j) != textFragment.charAt(j)) {
+                        break;
+                    }
+                }
+                return i;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -9,7 +26,13 @@ public class RabinKarp {
      * @param text text to find hash for
      * @return hash value of text
      */
-    static long hash(String text) {
-        return 0;
+    private static long getHash(String text) {
+        long hashValue = 0;
+        final int PRIME = 193;
+        final int RADIX = 10;
+        for (int i = 0; i < text.length(); i++) {
+            hashValue = (RADIX * (hashValue + text.charAt(i)) % PRIME);
+        }
+        return hashValue;
     }
 }
--- tests/RabinKarpTest.java
+++ tests/RabinKarpTest.java
@@ -1,6 +1,11 @@
-public class RabinKarpRunner {
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class RabinKarpTest {
     public static final String WIKIPEDIA = "In computer science, the Rabin–Karp algorithm or Karp–Rabin algorithm is a string-searching algorithm created by Richard M. Karp and Michael O. Rabin (1987) that uses hashing to find any one of a set of pattern strings in a text. For text of length n and p patterns of combined length m, its average and best case running time is O(n+m) in space O(p), but its worst-case time is O(nm). In contrast, the Aho–Corasick string-matching algorithm has asymptotic worst-time complexity O(n+m) in space O(m). A practical application of the algorithm is detecting plagiarism. Given source material, the algorithm can rapidly search through a paper for instances of sentences from the source material, ignoring details such as case and punctuation. Because of the abundance of the sought strings, single-string searching algorithms are impractical.";
-    public static void main(String[] args) {
-        System.out.println(RabinKarp.indexOf("science", WIKIPEDIA));
+    @Test
+    public void simpleWikipediaTest() {
+        assertEquals(WIKIPEDIA.indexOf("science"), RabinKarp.indexOf("science", WIKIPEDIA));
     }
+
 }
